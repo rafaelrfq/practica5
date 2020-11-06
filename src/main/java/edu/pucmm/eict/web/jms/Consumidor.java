d4fcba3c23faf93204dbcbe9954047b608d5b595
Rafael <rafaelrfq@gmail.com>
2020-11-05 23:17:39 -0500

    Added routes, templates and graphs

--- src/main/java/edu/pucmm/eict/web/jms/Consumidor.java
+++ src/main/java/edu/pucmm/eict/web/jms/Consumidor.java
@@ -1,13 +1,10 @@
 package edu.pucmm.eict.web.jms;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import javax.jms.*;
-import java.time.LocalDateTime;
 
 public class Consumidor {
     MensajeServices mensajeServices;
@@ -37,15 +34,15 @@ public class Consumidor {
                 TextMessage msg = (TextMessage) message;
                 System.out.println("Mensaje recibido: \n\n" + msg.getText());
                 ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.registerModule(new JavaTimeModule());
                 String json = msg.getText();
-                JsonNode jsonNode = objectMapper.readTree(json);
-                MensajeJson mensaje = new MensajeJson();
-                mensaje.setFechaGeneracion(LocalDateTime.parse(jsonNode.get("fechaGeneracion").asText()));
-                mensaje.setIdDispositivo(Integer.parseInt(jsonNode.get("idDispositivo").asText()));
-                mensaje.setHumedad(Float.parseFloat(jsonNode.get("humedad").asText()));
-                mensaje.setTemperatura(Float.parseFloat(jsonNode.get("temperatura").asText()));
-                // MensajeJson mensaje = objectMapper.readValue(msg.getText(), MensajeJson.class);
-                System.out.println(mensaje.getFechaGeneracion().toString());
+//                JsonNode jsonNode = objectMapper.readTree(json);
+//                MensajeJson mensaje = new MensajeJson();
+//                mensaje.setFechaGeneracion(LocalDateTime.parse(jsonNode.get("fechaGeneracion").asText()));
+//                mensaje.setIdDispositivo(Integer.parseInt(jsonNode.get("idDispositivo").asText()));
+//                mensaje.setHumedad(Float.parseFloat(jsonNode.get("humedad").asText()));
+//                mensaje.setTemperatura(Float.parseFloat(jsonNode.get("temperatura").asText()));
+                MensajeJson mensaje = objectMapper.readValue(json, MensajeJson.class);
                 mensajeServices.crearMensaje(mensaje);
             } catch (Exception e) {
                 e.printStackTrace();
--- src/main/java/edu/pucmm/eict/web/jms/MensajeController.java
+++ src/main/java/edu/pucmm/eict/web/jms/MensajeController.java
@@ -6,14 +6,50 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
 public class MensajeController {
     @Autowired MensajeServices mensajeServices;
 
-    @GetMapping("/")
+    @GetMapping("")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("list")
     public String listarMensajes(Model model){
         model.addAttribute("mensajes", mensajeServices.findAllMensajes());
         return "list";
+    }
+
+    @GetMapping("graphs")
+    public String graficas(Model model){
+        List<Mensaje> listado = mensajeServices.findAllMensajes();
+        float tempDisp1[], tempDisp2[], humDisp1[], humDisp2[];
+        int indiceDisp1 = 0, indiceDisp2 = 0;
+        tempDisp1 = new float[]{0,0,0,0,0};
+        tempDisp2 = new float[]{0,0,0,0,0};
+        humDisp1 = new float[]{0,0,0,0,0};
+        humDisp2 = new float[]{0,0,0,0,0};
+
+        for (Mensaje m : listado) {
+            if (m.getIdDispositivo() == 1){
+                tempDisp1[indiceDisp1] = m.getTemperatura();
+                humDisp1[indiceDisp1] = m.getHumedad();
+                indiceDisp1++;
+            } else if (m.getIdDispositivo() == 2){
+                tempDisp2[indiceDisp2] = m.getTemperatura();
+                humDisp2[indiceDisp2] = m.getHumedad();
+                indiceDisp2++;
+            }
+        }
+
+        model.addAttribute("tempDisp1", tempDisp1);
+        model.addAttribute("tempDisp2", tempDisp2);
+        model.addAttribute("humDisp1", humDisp1);
+        model.addAttribute("humDisp2", humDisp2);
+        return "graphs";
     }
 }
